2bce3f2fc10ef3fd6b50433edc0d9173074b4ccd
Fernando Chávez Boza <chavezbozafer@gmail.com>
2026-03-30 16:11:03 -0500

    Actualicé mi página4

--- app/page.tsx
+++ app/page.tsx
@@ -380,11 +380,9 @@ export default function Home() {
       <div className="flex flex-col sm:flex-row gap-4 justify-center">
 
         {/* EMAIL */}
-        <a
-  href="mailto:lchavezbocv@gmail.com?subject=Contacto%20desde%20portafolio&body=Hola%20Luis,%20quiero%20trabajar%20contigo"
->
-  Enviar correo
-</a>
+        <p className="px-8 py-4 bg-amber-400 text-black rounded-full font-bold text-sm">
+  lchavezbocv@gmail.com
+</p>
         {/* WHATSAPP 🔥 */}
         <a
           href="https://wa.me/51920994640"
